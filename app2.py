d7de750358415782ad5a8a3cc1c37623b297f5a7
franco153017 <161615342+franco153017@users.noreply.github.com>
2025-02-10 17:28:34 -0300

    Add files via upload

--- app2.py
+++ app2.py
@@ -7,33 +7,39 @@ def jogo_da_forca():
     #escolhe uma palavra aleatoria da lista
     palavra = random.choice(palavras)
     #cria uma lista para armazenar as letras corretas
-    letra_corretas = ['_'] * len(palavra)
+    letras_corretas = ['_'] * len(palavra)
     #cria uma lista para armazebar as letras erradas
     letras_erradas = []
-    #defina o numero de tentativas
+    #defina o numero de tentativa
     tentativa = 6
 
     print("bem-vindo ao jogo da forca!")
     print("você tem 6 tentativas para adivinhar a palavra. ")
 
-    while tentativas > 0 and '_' in letra_corretas:
-        #mostrar as palavras com as letra corretas
-        print(''.join(letra_corretas))
-        #pede para o usuario digitar uma letra
+    while tentativa > 0 and '_' in letras_corretas:
+        # mostrar as palavras com as letra corretas
+        print(''.join(letras_corretas))
+        # pede para o usuario digitar uma letra
         letra = input ("digite uma letra: ").lower()
-        #vereficar se a letra e correta
+        # verefica se a letra e correta
         if letra in  palavra:
-            #subistitui as letras corretas na lista
+            # subistitui as letras corretas na lista
             for i, l in enumerate(palavra):
                 if l == letra:
-                    letra_corretas[i] = letra
-            else:
-                #adiciona a letra errada a lista
+                    letras_corretas[i] = letra
+        else:
+                # adiciona a letra errada a lista
                 letras_erradas.append(letra)
-                #diminui o numero de tentativas
+                # diminui o numero de tentativas
                 tentativa -= 1
         print(f"tentativas restantes: {tentativa}")
         print(f"letras erradas:{','.join(letras_erradas)}")
  
- #vereficar se o usuario ganhou ou perdeu
- if '_' not in letras_corretas
+    # verefica se o usuario ganhou ou perdeu
+    if '_' not in letras_corretas:
+        print("Parabens! você ganhou!")
+    else:
+        print (f"Você perdeu! A palavra era {palavra}.")
+
+# inicia o jogo  
+# jogo_da_forca()
