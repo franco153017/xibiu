ea4d5be622b0c8a8af64297bdf108df66340e941
franco153017 <161615342+franco153017@users.noreply.github.com>
2025-02-12 15:56:20 -0300

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,58 @@
+#tic tac toe game in Python
+
+board = [' ' for _ in range(9)] #initialize an empty board
+
+def print_board():
+    row1 = '| {} | {} | {} |'.format(board[0], board [1], board [2])
+    row2 = '| {} | {} | {} |'.format(board[3], board [4], board [5])
+    row3 = '| {} | {} | {} |'.format(board[6], board [7], board [8])
+
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+
+def player_move(icon):
+    if icon == 'x':
+            number = 1
+    elif icon == 'o':
+            number = 2
+
+            print("Your turn player {}".format(number)) 
+
+            choice=int(input("Enter you mover (1-9):").strip())
+    if board[choice - 1] == ' ':
+            board[choice - 1] == icon
+    else:
+        print()
+        print("That space is taken!")
+
+def is_victory(icon):
+    if (board[0] == icon and board[1] == icon and board[2] == icon) or \
+        (board[3] == icon and board[4] == icon and board[5] == icon) or \
+        (board[6] == icon and board[7] == icon and board[8] == icon) or \
+        (board[0] == icon and board[3] == icon and board[6] == icon) or \
+        (board[1] == icon and board[4] == icon and board[7] == icon) or \
+        (board[2] == icon and board[5] == icon and board[8] == icon) or \
+        (board[0] == icon and board[4] == icon and board[8] == icon) or \
+        (board[2] == icon and board[4] == icon and board[6] == icon):
+        return True
+    else:
+        return False
+
+while True:
+    print_board()
+    player_move('X')
+    print_board
+    if is_victory('X'):
+        print("player 1 wins!! Congratulations!" )
+        break
+    elif ' ' not in board:
+        print("It's a tie!")
+        break
+    player_move('O')
+    if is_victory('O'):
+        print_board()
+        print("Player 2 wins congratulations!")
+        break
